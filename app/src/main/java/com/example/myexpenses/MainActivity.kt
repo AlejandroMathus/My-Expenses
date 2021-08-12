cf17e4d50021c237502ad8d3593c39ab7cbf793c
Alejandro <alejandrojoseml94@gmail.com>
2021-08-12 11:56:09 -0300

    Colors added

--- app/src/main/java/com/example/myexpenses/MainActivity.kt
+++ app/src/main/java/com/example/myexpenses/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myexpenses
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
